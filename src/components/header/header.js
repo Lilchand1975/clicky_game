177952233e1fd43cc600d7440a7fd9e2ac2cbe8c
chandra Chhetri <chhetricc@gmail.com>
2018-09-26 19:35:41 -0400

    Updates

--- src/components/header/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./header.css";
-
-const Header = props => (<header className="header">
-                            <h1></h1>
-                        </header>
-);
-
-export default Header;
